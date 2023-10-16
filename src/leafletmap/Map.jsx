6ee3e757eaccef65a9aa8405ed3328ae809774ec
kelvin nzioka <kkelvinnzioka@gmail.com>
2023-10-16 15:19:00 +0300

    center kutus map

--- src/leafletmap/Map.jsx
+++ src/leafletmap/Map.jsx
@@ -1,30 +1,24 @@
+import React from "react";
 import LocationMarker from "./LocationMarker";
 import "./map.css";
-import { MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
 
 const Map = () => {
-  const position = [51.505, -0.09];
+  const kutusCoordinates = [-0.679887, 37.171047]; // Latitude and Longitude for Kutus, Kenya
 
   return (
-    <>
-      <MapContainer
-        className="map"
-        center={position}
-        zoom={13}
-        scrollWheelZoom={false}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-        <LocationMarker />
-      </MapContainer>
-    </>
+    <MapContainer
+      className="map"
+      center={kutusCoordinates}
+      zoom={13}
+      scrollWheelZoom={false}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <LocationMarker />
+    </MapContainer>
   );
 };
 
